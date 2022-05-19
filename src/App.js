7dc11b055448ddd56a426f1447bc87f3704d9dfa
Jannelex Karlsson <jannelex.karlsson@outlook.com>
2022-05-19 09:55:59 +0200

    Snart klar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Form from './Components/Form';
 import TodoList from './Components/Todolist';
 
 function App() {
+
   const[inputText, setInputText] = useState("");
   const[todos, setTodos] = useState([]);
   const[status, setStatus] = useState("all");
+  const[filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch(status){
+      case "completed":
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case "uncompleted":
+         setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  
   return (
     <div className="App">
      <header>
@@ -14,11 +36,17 @@ function App() {
     </header>
     <Form 
     inputText={inputText} 
-    todos={todos} setTodos={setTodos} 
+    todos={todos} 
+    setTodos={setTodos} 
     setInputText={setInputText}
     setStatus={setStatus}
+    
     />
-    <TodoList todos={todos} setTodos={setTodos}/>
+    <TodoList 
+    filteredTodos={filteredTodos}
+    setTodos={setTodos}
+    todos={todos} 
+    />
     </div>
   );
 }
--- src/Components/Todolist.js
+++ src/Components/Todolist.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Todo from "./Todo";
-const TodoList = ({todos, setTodos}) => {
+const TodoList = ({todos, setTodos, filteredTodos}) => {
     return (
     <div className="todo-container">
         <ul className="todo-list">
-            {todos.map((todo) => (
+            {filteredTodos.map((todo) => (
                 <Todo 
-                todos={todos} 
                 setTodos={setTodos} 
+                todos={todos} 
                 key={todo.id} 
                 todo={todo}
                 text={todo.text} 
+
                 />
             ))}
         </ul>
